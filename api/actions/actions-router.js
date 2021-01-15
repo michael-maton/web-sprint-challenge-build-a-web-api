a2c0be8495f7263cce4ba48cacea8b7bec133c0b
Michael Maton <mjmaton6@gmail.com>
2021-01-14 20:03:52 -0600

    actions GET/POST complete, more middleware created

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,5 +3,38 @@ const express = require("express");
 const router = express.Router();
 const Actions = require("./actions-model");
 
+const { validateActionId, validateAction } = require("../middleware/");
+
+// GET
+// GET
+// GET
+router.get("/", (req, res) => {
+  Actions.get()
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ error: "There was an error retrieving the actions" });
+    });
+});
+
+router.get("/:id", validateActionId, (req, res) => {
+  res.status(200).json(req.action);
+});
+
+// POST
+// POST
+// POST
+router.post("/", validateAction, (req, res) => {
+  Actions.insert(req.body)
+    .then((action) => {
+      res.status(201).json(req.body);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "There was an error creating the action" });
+    });
+});
 
 module.exports = router;
--- api/middleware/index.js
+++ api/middleware/index.js
@@ -10,4 +10,29 @@ function logger(req, res, next) {
   next();
 }
 
-module.exports = { logger };
+async function validateActionId(req, res, next) {
+  console.log("checking action id");
+  try {
+    const action = await Actions.get(req.params.id);
+    if (action) {
+      req.action = action;
+      console.log(`Action id ${req.params.id} found`);
+      next();
+    } else {
+      console.log(`Action with id ${req.params.id} not found`);
+      res.status(404).json(`Action with id ${req.params.id} not found`);
+    }
+  } catch (error) {
+    res.status(500).json("error");
+  }
+}
+
+function validateAction(req, res, next) {
+  console.log("Validating action fields");
+  const { project_id, description, notes } = req.body;
+  project_id && description && notes
+    ? next()
+    : res.status(400).json({ error: "Please fill out required fields" });
+}
+
+module.exports = { logger, validateActionId, validateAction };
